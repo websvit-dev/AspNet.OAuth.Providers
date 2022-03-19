b1057891d051214ab2845eb684fe525ba86a5a85
Vitaliy Basanets <w.basanets2301@gmail.com>
2022-03-19 16:57:13 +0200

    added option models

---
+++ AspNet.OAuth.Providers/Options/OAuthOptionsDictionary.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNet.OAuth.Providers.Options
+{
+    public class OAuthOptionsDictionary : Dictionary<string, OAuthOptions>
+    {
+    }
+}
