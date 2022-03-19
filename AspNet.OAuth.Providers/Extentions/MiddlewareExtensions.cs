e9d12a1c2d6f8f9ade4e13f2df8de69b29d8598e
Vitaliy Basanets <w.basanets2301@gmail.com>
2022-03-19 19:36:55 +0200

    added authentication builder and extentions

---
+++ AspNet.OAuth.Providers/Extentions/MiddlewareExtensions.cs
@@ -0,0 +1,23 @@
+﻿using AspNet.OAuth.Providers.Middlewares;
+using AspNet.OAuth.Providers.Store;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Rewrite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static AspNet.OAuth.Providers.Constants.Constants;
+
+namespace AspNet.OAuth.Providers.Extentions
+{
+    public static class MiddlewareExtensions
+    {
+        public static void UseSocialAuthentication(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<AuthenticationMiddleware>();
+
+            var options = new RewriteOptions()
+                            .AddRewrite(Defaults.SOCIAL_AUTHENTICATION_ENDPOINT, SocialStore.CallbackEndpoint, true);
+            app.UseRewriter(options);
+        }
+    }
+}
